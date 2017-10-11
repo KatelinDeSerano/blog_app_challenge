f0a9c26acb5244727c28c6832d70a01ee8fb26d3
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-10-10 19:57:08 -0500

    added put endpoint

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const blogPostsRouter = require('./blogRouter');
 const app = express();
 
 app.use(morgan('common'));
+
+app.use('/blog-posts', blogRouter);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
