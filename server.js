30ca43382babd10f05422bdb4e455f6d186f962c
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-10-10 20:53:31 -0500

    added dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const blogPostsRouter = require('./blogRouter');
+const blogRouter = require('./blogRouter');
 const app = express();
 
 app.use(morgan('common'));
