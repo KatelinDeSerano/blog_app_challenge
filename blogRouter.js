4a9dab315b33e9dfb5158bea542cc26a44d92962
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-10-10 19:43:07 -0500

    made post request

--- blogRouter.js
+++ blogRouter.js
@@ -13,3 +13,20 @@ BlogPosts.create('What Financial Advice Millienials Should be Following (But Are
 router.get('/', (req, res) => {
     res.json(BlogPosts.get());
   });
+
+  router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    const item = BlogPosts.create(
+        req.body.title, req.body.content, req.body.author);
+    res.status(201).json(item);
+  });
+
+  
